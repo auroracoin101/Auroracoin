3e9cd2bb21afa2983c9353752c273172d181cbd0
Myckel Habets <myckel@sdf.lonestar.org>
2019-12-16 13:42:36 +0100

    Bitcoin: fc4db35bfd78d85d6b52d5da3d89696160658450 (wallet: Add ListWalletDir utility).

--- src/wallet/walletutil.h
+++ src/wallet/walletutil.h
@@ -1,15 +1,20 @@
 // Copyright (c) 2009-2019 The Bitcoin Core developers
 // Copyright (c) 2014-2019 The DigiByte Core developers
+// Copyright (c) 2014-2019 The Auroracoin developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #ifndef AURORACOIN_WALLET_WALLETUTIL_H
 #define AURORACOIN_WALLET_WALLETUTIL_H
 
-#include <chainparamsbase.h>
-#include <util.h>
+#include <fs.h>
+
+#include <vector>
 
 //! Get the path of the wallet directory.
 fs::path GetWalletDir();
+
+//! Get wallets in wallet directory.
+std::vector<fs::path> ListWalletDir();
 
 #endif // AURORACOIN_WALLET_WALLETUTIL_H
