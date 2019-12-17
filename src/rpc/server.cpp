eacfc4087571977175ba20f8d2a4ef72675ea91d
Myckel Habets <myckel@sdf.lonestar.org>
2019-12-17 09:59:12 +0100

    Bitcoin: cb53b825c26af6e628ba88d72b2000e75bedbbc6 (scripted-diff: Replace boost::bind with std::bind).

--- src/rpc/server.cpp
+++ src/rpc/server.cpp
@@ -15,7 +15,6 @@
 #include <util.h>
 #include <utilstrencodings.h>
 
-#include <boost/bind.hpp>
 #include <boost/signals2/signal.hpp>
 #include <boost/algorithm/string/classification.hpp>
 #include <boost/algorithm/string/split.hpp>
