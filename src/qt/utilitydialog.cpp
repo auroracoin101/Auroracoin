1ae0a2e3679c60c50d21d51f7a588d5ee7ad859f
Myckel Habets <myckel@sdf.org>
2020-01-16 10:00:43 +0100

    Bitcoin: 60ae463a68206b08faaadfbb023d7ede78fff784 (Fix 'bitcoin-qt -version' output formatting).

--- src/qt/utilitydialog.cpp
+++ src/qt/utilitydialog.cpp
@@ -25,6 +25,7 @@
 #include <init.h>
 #include <interfaces/node.h>
 #include <util/system.h>
+#include <util/strencodings.h>
 
 #include <stdio.h>
 
@@ -56,9 +57,9 @@ HelpMessageDialog::HelpMessageDialog(interfaces::Node& node, QWidget *parent, bo
     {
         setWindowTitle(tr("About %1").arg(tr(PACKAGE_NAME)));
 
+        std::string licenseInfo = LicenseInfo();
         /// HTML-format the license message from the core
-        QString licenseInfo = QString::fromStdString(LicenseInfo());
-        QString licenseInfoHTML = licenseInfo;
+        QString licenseInfoHTML = QString::fromStdString(LicenseInfo());
         // Make URLs clickable
         QRegExp uri("<(.*)>", Qt::CaseSensitive, QRegExp::RegExp2);
         uri.setMinimal(true); // use non-greedy matching
@@ -68,7 +69,7 @@ HelpMessageDialog::HelpMessageDialog(interfaces::Node& node, QWidget *parent, bo
 
         ui->aboutMessage->setTextFormat(Qt::RichText);
         ui->scrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
-        text = version + "\n" + licenseInfo;
+        text = version + "\n" + QString::fromStdString(FormatParagraph(licenseInfo));
         ui->aboutMessage->setText(version + "<br><br>" + licenseInfoHTML);
         ui->aboutMessage->setWordWrap(true);
         ui->helpMessage->setVisible(false);
