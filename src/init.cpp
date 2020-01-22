f8d68ff4f906a66cc09b5b449d81f6127fd4e020
Myckel Habets <myckel@sdf.org>
2020-01-22 14:04:23 +0100

    Bitcoin: 8db11dd0b182a93042899651545cc21b34bf0742 (Pass chain and client variables where needed).

--- src/init.cpp
+++ src/init.cpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2018 The DigiByte Core developers
+// Copyright (c) 2009-2018 The Bitcoin Core developers
 // Copyright (c) 2014-2019 The Auroracoin developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
@@ -33,6 +33,7 @@
 #include <rpc/server.h>
 #include <rpc/register.h>
 #include <rpc/blockchain.h>
+#include <rpc/util.h>
 #include <script/standard.h>
 #include <script/sigcache.h>
 #include <scheduler.h>
@@ -157,7 +158,7 @@ void Interrupt()
     }
 }
 
-void Shutdown()
+void Shutdown(InitInterfaces& interfaces)
 {
     LogPrintf("%s: In progress...\n", __func__);
     static CCriticalSection cs_Shutdown;
@@ -1169,7 +1170,7 @@ bool AppInitLockDataDirectory()
     return true;
 }
 
-bool AppInitMain()
+bool AppInitMain(InitInterfaces& interfaces)
 {
     const CChainParams& chainparams = Params();
     // ********************************************************* Step 4a: application initialization
@@ -1237,6 +1238,7 @@ bool AppInitMain()
      */
     RegisterAllCoreRPCCommands(tableRPC);
     g_wallet_init_interface.RegisterRPC(tableRPC);
+    g_rpc_interfaces = &interfaces;
 #if ENABLE_ZMQ
     RegisterZMQRPCCommands(tableRPC);
 #endif
@@ -1254,7 +1256,7 @@ bool AppInitMain()
     }
 
     // ********************************************************* Step 5: verify wallet database integrity
-    if (!g_wallet_init_interface.Verify()) return false;
+    if (!g_wallet_init_interface.Verify(*interfaces.chain)) return false;
 
     // ********************************************************* Step 6: network initialization
     // Note that we absolutely cannot open any actual connections
@@ -1579,7 +1581,7 @@ bool AppInitMain()
     }
 
     // ********************************************************* Step 9: load wallet
-    if (!g_wallet_init_interface.Open()) return false;
+    if (!g_wallet_init_interface.Open(*interfaces.chain)) return false;
 
     // ********************************************************* Step 10: data directory maintenance
 
--- src/wallet/test/init_test_fixture.h
+++ src/wallet/test/init_test_fixture.h
@@ -5,6 +5,7 @@
 #ifndef AURORACOIN_WALLET_TEST_INIT_TEST_FIXTURE_H
 #define AURORACOIN_WALLET_TEST_INIT_TEST_FIXTURE_H
 
+#include <interfaces/chain.h>
 #include <test/test_auroracoin.h>
 
 
@@ -16,6 +17,7 @@ struct InitWalletDirTestingSetup: public BasicTestingSetup {
     fs::path m_datadir;
     fs::path m_cwd;
     std::map<std::string, fs::path> m_walletdir_path_cases;
+    std::unique_ptr<interfaces::Chain> m_chain = interfaces::MakeChain();
 };
 
 #endif // AURORACOIN_WALLET_TEST_INIT_TEST_FIXTURE_H
