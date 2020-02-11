ac947858116bc02ac8ef41c16f3412fb856a4524
Myckel Habets <myckel@sdf.org>
2020-02-11 16:13:50 +0100

    Bitcoin: 02e1e4eff6cda0bfc24b455a7c1583394cbff6eb (rpc: Add wait argument to stop).

--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -167,6 +167,7 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "rescanblockchain", 1, "stop_height"},
     { "createwallet", 1, "disable_private_keys"},
     { "getnodeaddresses", 0, "count"},
+    { "stop", 0, "wait" },
 };
 // clang-format on
 
