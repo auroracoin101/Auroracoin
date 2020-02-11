7f67f53fb7aafe55c05412388272404909d3bc65
Myckel Habets <myckel@sdf.org>
2020-02-11 11:09:00 +0100

    Bitcoin: fa21ca09a807e18476babe2651ad62df6477bbf0 (test: Add BOOST_REQUIRE to getters returning optimal).

--- src/test/coins_tests.cpp
+++ src/test/coins_tests.cpp
@@ -4,17 +4,18 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#include <attributes.h>
 #include <coins.h>
+#include <consensus/validation.h>
 #include <script/standard.h>
+#include <test/test_auroracoin.h>
 #include <uint256.h>
 #include <undo.h>
 #include <util/strencodings.h>
-#include <test/test_auroracoin.h>
 #include <validation.h>
-#include <consensus/validation.h>
 
-#include <vector>
 #include <map>
+#include <vector>
 
 #include <boost/test/unit_test.hpp>
 
@@ -38,7 +39,7 @@ class CCoinsViewTest : public CCoinsView
     std::map<COutPoint, Coin> map_;
 
 public:
-    bool GetCoin(const COutPoint& outpoint, Coin& coin) const override
+    NODISCARD bool GetCoin(const COutPoint& outpoint, Coin& coin) const override
     {
         std::map<COutPoint, Coin>::const_iterator it = map_.find(outpoint);
         if (it == map_.end()) {
