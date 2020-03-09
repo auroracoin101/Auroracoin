1677cbf4bc092734202367d16262d972c50313a9
Myckel Habets <myckel@sdf.org>
2020-03-09 10:55:53 +0100

    Bitcoin: 8b9171ccf0a90583b5f3802ac3b4b212c230e0ed (wallet: Initialize stop_block to nullptr in CWallet::ScanForWalletTransactions).

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -1604,6 +1604,7 @@ CWallet::ScanResult CWallet::ScanForWalletTransactions(const CBlockIndex* const 
 
     const CBlockIndex* pindex = pindexStart;
     failed_block = nullptr;
+    stop_block = nullptr;
 
     if (pindex) WalletLogPrintf("Rescan started from block %d...\n", pindex->nHeight);
 
