1a7c5a066cd232a169e760994cec529235304832
Myckel Habets <myckel@sdf.org>
2020-03-09 14:07:36 +0100

    Bitcoin: b301950df32443e358bc22ca22c6f9ac09d18219 (Made expicit constructor CTransaction(const CMutableTransaction &tx).).

--- src/primitives/transaction.h
+++ src/primitives/transaction.h
@@ -299,7 +299,7 @@ public:
     CTransaction();
 
     /** Convert a CMutableTransaction into a CTransaction. */
-    CTransaction(const CMutableTransaction &tx);
+    explicit CTransaction(const CMutableTransaction &tx);
     CTransaction(CMutableTransaction &&tx);
 
     template <typename Stream>
