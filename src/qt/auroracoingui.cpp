760e0bb9aca5719ae1033e90e2916dcbf09505ed
Myckel Habets <myckel@sdf.org>
2020-04-07 13:22:37 +0200

    Bitcoin: 8847cdaaaeb45c1ddee89f43ac4b8fafb20e5c0d (gui: Add OpenWalletActivity).

--- src/qt/auroracoingui.cpp
+++ src/qt/auroracoingui.cpp
@@ -376,7 +376,9 @@ void AuroracoinGUI::createActions()
                 QString name = path.empty() ? QString("["+tr("default wallet")+"]") : QString::fromStdString(path);
                 QAction* action = m_open_wallet_action->menu()->addAction(name);
                 connect(action, &QAction::triggered, [this, path] {
-                    setCurrentWallet(m_wallet_controller->openWallet(path));
+                    OpenWalletActivity* activity = m_wallet_controller->openWallet(path);
+                    connect(activity, &OpenWalletActivity::opened, this, &AuroracoinGUI::setCurrentWallet);
+                    connect(activity, &OpenWalletActivity::finished, activity, &QObject::deleteLater);
                 });
             }
         });
