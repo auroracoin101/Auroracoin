a73932ecca03cf9382f957625ce917eb2bc9d798
Myckel Habets <myckel@sdf.org>
2020-04-09 11:17:13 +0200

    Make gen-manpages.sh usable for auroracoin and Bitcoin: fa0fe3b8a4425eb5f38e26ccbe42489a6d591e43 (contrib: Add missing wallet tool to gen-manpages.sh).

--- contrib/devtools/gen-manpages.sh
+++ contrib/devtools/gen-manpages.sh
@@ -7,26 +7,27 @@ BUILDDIR=${BUILDDIR:-$TOPDIR}
 BINDIR=${BINDIR:-$BUILDDIR/src}
 MANDIR=${MANDIR:-$TOPDIR/doc/man}
 
-DIGIBYTED=${DIGIBYTED:-$BINDIR/digibyted}
-DIGIBYTECLI=${DIGIBYTECLI:-$BINDIR/digibyte-cli}
-DIGIBYTETX=${DIGIBYTETX:-$BINDIR/digibyte-tx}
-DIGIBYTEQT=${DIGIBYTEQT:-$BINDIR/qt/digibyte-qt}
+AURORACOIND=${AURORACOIND:-$BINDIR/auroracoind}
+AURORACOINCLI=${AURORACOINCLI:-$BINDIR/auroracoin-cli}
+AURORACOINTX=${AURORACOINTX:-$BINDIR/auroracoin-tx}
+WALLET_TOOL=${WALLET_TOOL:-$BINDIR/auroracoin-wallet}
+AURORACOINQT=${AURORACOINQT:-$BINDIR/qt/auroracoin-qt}
 
-[ ! -x $DIGIBYTED ] && echo "$DIGIBYTED not found or not executable." && exit 1
+[ ! -x $AURORACOIND ] && echo "$AURORACOIND not found or not executable." && exit 1
 
 # The autodetected version git tag can screw up manpage output a little bit
-DGBVER=($($DIGIBYTECLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }'))
+AURVER=($($AURORACOINCLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }'))
 
 # Create a footer file with copyright content.
-# This gets autodetected fine for digibyted if --version-string is not set,
-# but has different outcomes for digibyte-qt and digibyte-cli.
+# This gets autodetected fine for auroracoind if --version-string is not set,
+# but has different outcomes for auroracoin-qt and auroracoin-cli.
 echo "[COPYRIGHT]" > footer.h2m
-$DIGIBYTED --version | sed -n '1!p' >> footer.h2m
+$AURORACOIND --version | sed -n '1!p' >> footer.h2m
 
-for cmd in $DIGIBYTED $DIGIBYTECLI $DIGIBYTETX $DIGIBYTEQT; do
+for cmd in $AURORACOIND $AURORACOINCLI $AURORACOINTX $WALLET_TOOL $AURORACOINQT; do
   cmdname="${cmd##*/}"
-  help2man -N --version-string=${DGBVER[0]} --include=footer.h2m -o ${MANDIR}/${cmdname}.1 ${cmd}
-  sed -i "s/\\\-${DGBVER[1]}//g" ${MANDIR}/${cmdname}.1
+  help2man -N --version-string=${AURVER[0]} --include=footer.h2m -o ${MANDIR}/${cmdname}.1 ${cmd}
+  sed -i "s/\\\-${AURVER[1]}//g" ${MANDIR}/${cmdname}.1
 done
 
 rm -f footer.h2m
