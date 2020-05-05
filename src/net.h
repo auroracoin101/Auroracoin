3a5a1c790ac443a95514ef20afe8ae3a1c2f1da8
Myckel Habets <myckel@sdf.org>
2020-05-05 10:18:02 +0200

    Bitcoin: 9c1a607a092039ada85aa7a8c12c6062a8e89b94 (net: Rename ::fRelayTxes to ::g_relay_txes).

--- src/net.h
+++ src/net.h
@@ -559,8 +559,7 @@ CAddress GetLocalAddress(const CNetAddr *paddrPeer, ServiceFlags nLocalServices)
 
 extern bool fDiscover;
 extern bool fListen;
-extern bool fRelayTxes;
-
+extern bool g_relay_txes;
 extern limitedmap<uint256, int64_t> mapAlreadyAskedFor;
 
 /** Subversion as sent to the P2P network in `version` messages */
