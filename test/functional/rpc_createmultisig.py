85572d631101aa0a2defceacbc1e17c43fd286cb
Myckel Habets <myckel@sdf.org>
2020-05-08 10:02:00 +0200

    Bitcoin: 13b3bb564485b239822fb8bbdab7ce528f0f0d6f (test: Fixup creatmultisig documentation and whitespace).

--- test/functional/rpc_createmultisig.py
+++ test/functional/rpc_createmultisig.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-# Copyright (c) 2009-2019 The Bitcoin Core developers
+# Copyright (c) 2015-2019 The Bitcoin Core developers
 # Copyright (c) 2014-2019 The DigiByte Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
-"""Test transaction signing using the signrawtransaction* RPCs."""
+"""Test multisig RPCs"""
 
 from test_framework.test_framework import DigiByteTestFramework
 import decimal
@@ -18,21 +18,21 @@ class RpcCreateMultiSigTest(DigiByteTestFramework):
 
     def get_keys(self):
         node0, node1, node2 = self.nodes
-        self.add = [node1.getnewaddress() for _ in range(self.nkeys)]
-        self.pub = [node1.getaddressinfo(a)["pubkey"] for a in self.add]
-        self.priv = [node1.dumpprivkey(a) for a in self.add]
+        add = [node1.getnewaddress() for _ in range(self.nkeys)]
+        self.pub = [node1.getaddressinfo(a)["pubkey"] for a in add]
+        self.priv = [node1.dumpprivkey(a) for a in add]
         self.final = node2.getnewaddress()
 
     def run_test(self):
-        node0,node1,node2 = self.nodes
+        node0, node1, node2 = self.nodes
 
-        # 50 DGB each, rest will be 25 DGB each
+        # 50 AUR each, rest will be 25 AUR each
         node0.generate(149)
         self.sync_all()
 
         self.moved = 0
-        for self.nkeys in [3,5]:
-            for self.nsigs in [2,3]:
+        for self.nkeys in [3, 5]:
+            for self.nsigs in [2, 3]:
                 for self.output_type in ["bech32", "p2sh-segwit", "legacy"]:
                     self.get_keys()
                     self.do_multisig()
@@ -40,7 +40,7 @@ class RpcCreateMultiSigTest(DigiByteTestFramework):
         self.checkbalances()
 
     def checkbalances(self):
-        node0,node1,node2 = self.nodes
+        node0, node1, node2 = self.nodes
         node0.generate(100)
         self.sync_all()
 
@@ -50,13 +50,13 @@ class RpcCreateMultiSigTest(DigiByteTestFramework):
 
         height = node0.getblockchaininfo()["blocks"]
         assert 150 < height < 350
-        total = 149*50 + (height-149-100)*25
+        total = 149 * 50 + (height - 149 - 100) * 25
         assert bal1 == 0
         assert bal2 == self.moved
-        assert bal0+bal1+bal2 == total
+        assert bal0 + bal1 + bal2 == total
 
     def do_multisig(self):
-        node0,node1,node2 = self.nodes
+        node0, node1, node2 = self.nodes
 
         msig = node2.createmultisig(self.nsigs, self.pub, self.output_type)
         madd = msig["address"]
@@ -75,7 +75,7 @@ class RpcCreateMultiSigTest(DigiByteTestFramework):
         txid = node0.sendtoaddress(madd, 40)
 
         tx = node0.getrawtransaction(txid, True)
-        vout = [v["n"] for v in tx["vout"] if madd in v["scriptPubKey"].get("addresses",[])]
+        vout = [v["n"] for v in tx["vout"] if madd in v["scriptPubKey"].get("addresses", [])]
         assert len(vout) == 1
         vout = vout[0]
         scriptPubKey = tx["vout"][vout]["scriptPubKey"]["hex"]
@@ -87,7 +87,7 @@ class RpcCreateMultiSigTest(DigiByteTestFramework):
         outval = value - decimal.Decimal("0.00001000")
         rawtx = node2.createrawtransaction([{"txid": txid, "vout": vout}], [{self.final: outval}])
 
-        rawtx2 = node2.signrawtransactionwithkey(rawtx, self.priv[0:self.nsigs-1], prevtxs)
+        rawtx2 = node2.signrawtransactionwithkey(rawtx, self.priv[0:self.nsigs - 1], prevtxs)
         rawtx3 = node2.signrawtransactionwithkey(rawtx2["hex"], [self.priv[-1]], prevtxs)
 
         self.moved += outval
@@ -97,6 +97,7 @@ class RpcCreateMultiSigTest(DigiByteTestFramework):
 
         txinfo = node0.getrawtransaction(tx, True, blk)
         self.log.info("n/m=%d/%d %s size=%d vsize=%d weight=%d" % (self.nsigs, self.nkeys, self.output_type, txinfo["size"], txinfo["vsize"], txinfo["weight"]))
+
 
 if __name__ == '__main__':
     RpcCreateMultiSigTest().main()
