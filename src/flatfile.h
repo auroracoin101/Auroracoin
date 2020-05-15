5259c4e7621b13a9f9726c633bd6429f328c7f01
Myckel Habets <myckel@sdf.org>
2020-05-15 13:50:28 +0200

    Bitcoin: e2d2abb99fe353ffc2ff3bc1ff578fad31065335 (validation: Refactor OpenDiskFile into method on FlatFileSeq.).

--- src/flatfile.h
+++ src/flatfile.h
@@ -31,6 +31,9 @@ public:
 
     /** Get the name of the file at the given position. */
     fs::path FileName(const CDiskBlockPos& pos) const;
+
+    /** Open a handle to the file at the given position. */
+    FILE* Open(const CDiskBlockPos& pos, bool fReadOnly = false);
 };
 
 #endif // AURORACOIN_FLATFILE_H
