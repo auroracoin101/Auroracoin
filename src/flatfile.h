32ddd2c6e2aee9b1592d214a90b034da1a1f9e0e
Myckel Habets <myckel@sdf.org>
2020-05-15 15:06:54 +0200

    Bitcoin: e0380933e3745214331d358bda8c5e79299c84d2, d6d8a78f26f52fdfe43293686135e2fc6919926c and 65a489e93d181d3c0f7a9cf79f7c11ff8cf2b0f0 (CBlockDiskPos/FlatFilePos refactoring to flatfile.)

--- src/flatfile.h
+++ src/flatfile.h
@@ -1,12 +1,51 @@
-// Copyright (c) 2019 The Bitcoin Core developers
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2019 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #ifndef AURORACOIN_FLATFILE_H
 #define AURORACOIN_FLATFILE_H
 
-#include <chain.h>
+#include <string>
+
 #include <fs.h>
+#include <serialize.h>
+
+struct FlatFilePos
+{
+    int nFile;
+    unsigned int nPos;
+
+    ADD_SERIALIZE_METHODS;
+
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action) {
+        READWRITE(VARINT(nFile, VarIntMode::NONNEGATIVE_SIGNED));
+        READWRITE(VARINT(nPos));
+    }
+
+    FlatFilePos() {
+        SetNull();
+    }
+
+    FlatFilePos(int nFileIn, unsigned int nPosIn) {
+        nFile = nFileIn;
+        nPos = nPosIn;
+    }
+
+    friend bool operator==(const FlatFilePos &a, const FlatFilePos &b) {
+        return (a.nFile == b.nFile && a.nPos == b.nPos);
+    }
+
+    friend bool operator!=(const FlatFilePos &a, const FlatFilePos &b) {
+        return !(a == b);
+    }
+
+    void SetNull() { nFile = -1; nPos = 0; }
+    bool IsNull() const { return (nFile == -1); }
+
+    std::string ToString() const;
+};
 
 /**
  * FlatFileSeq represents a sequence of numbered files storing raw data. This class facilitates
@@ -30,10 +69,10 @@ public:
     FlatFileSeq(fs::path dir, const char* prefix, size_t chunk_size);
 
     /** Get the name of the file at the given position. */
-    fs::path FileName(const CDiskBlockPos& pos) const;
+    fs::path FileName(const FlatFilePos& pos) const;
 
     /** Open a handle to the file at the given position. */
-    FILE* Open(const CDiskBlockPos& pos, bool fReadOnly = false);
+    FILE* Open(const FlatFilePos& pos, bool fReadOnly = false);
 
     /**
      * Allocate additional space in a file after the given starting position. The amount allocated
@@ -44,7 +83,16 @@ public:
      * @param[out] out_of_space Whether the allocation failed due to insufficient disk space.
      * @return The number of bytes successfully allocated.
      */
-    size_t Allocate(const CDiskBlockPos& pos, size_t add_size, bool& out_of_space);
+    size_t Allocate(const FlatFilePos& pos, size_t add_size, bool& out_of_space);
+
+    /**
+     * Commit a file to disk, and optionally truncate off extra pre-allocated bytes if final.
+     *
+     * @param[in] pos The first unwritten position in the file to be flushed.
+     * @param[in] finalize True if no more data will be written to this file.
+     * @return true on success, false on failure.
+     */
+    bool Flush(const FlatFilePos& pos, bool finalize = false);
 };
 
 #endif // AURORACOIN_FLATFILE_H
