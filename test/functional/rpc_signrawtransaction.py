dbb61547ff27b374bfdab1e8e6db0f2992481f62
Myckel Habets <myckel@sdf.org>
2020-05-26 22:11:52 +0200

    Bitcoin: 839c3f7c4937eb8a3e1e5ab2dafff26688af1078 ([rpc] Remove signrawtransaction warning).

--- test/functional/rpc_signrawtransaction.py
+++ test/functional/rpc_signrawtransaction.py
@@ -16,7 +16,6 @@ class SignRawTransactionsTest(DigiByteTestFramework):
     def set_test_params(self):
         self.setup_clean_chain = True
         self.num_nodes = 2
-        self.extra_args = [["-deprecatedrpc=signrawtransaction"], []]
 
     def skip_test_if_missing_module(self):
         self.skip_if_no_wallet()
