402ad72c4abbb438d02d95363c502543ba66eb82
Myckel Habets <myckel@sdf.org>
2020-06-16 10:28:24 +0200

    Bitcoin: a014373d81534c9df4a2cbac0f74181e708da3cc (QA: Avoid re-reading config.ini unnecessarily).

--- test/functional/test_framework/test_framework.py
+++ test/functional/test_framework/test_framework.py
@@ -557,22 +557,13 @@ class AuroracoinTestFramework(metaclass=AuroracoinTestMetaClass):
             raise SkipTest("digibyte-cli has not been compiled.")
 
     def is_cli_compiled(self):
-        """Checks whether digibyte-cli was compiled."""
-        config = configparser.ConfigParser()
-        config.read_file(open(self.options.configfile))
-
-        return config["components"].getboolean("ENABLE_CLI")
+        """Checks whether auroracoin-cli was compiled."""
+        return self.config["components"].getboolean("ENABLE_CLI")
 
     def is_wallet_compiled(self):
         """Checks whether the wallet module was compiled."""
-        config = configparser.ConfigParser()
-        config.read_file(open(self.options.configfile))
-
-        return config["components"].getboolean("ENABLE_WALLET")
+        return self.config["components"].getboolean("ENABLE_WALLET")
 
     def is_zmq_compiled(self):
         """Checks whether the zmq module was compiled."""
-        config = configparser.ConfigParser()
-        config.read_file(open(self.options.configfile))
-
-        return config["components"].getboolean("ENABLE_ZMQ")
+        return self.config["components"].getboolean("ENABLE_ZMQ")
