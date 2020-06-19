832785930f9eaddb0016a3b45e5786170c9cf655
Myckel Habets <myckel@sdf.org>
2020-06-19 10:01:17 +0200

    Bitcoin: ae5f2b6a6cc7b2260e9dff99c1bf378922e0e988 (threads: introduce util/threadnames, refactor thread naming).

--- src/validation.cpp
+++ src/validation.cpp
@@ -49,6 +49,7 @@
 
 #include <future>
 #include <sstream>
+#include <string>
 
 #include <boost/algorithm/string/replace.hpp>
 #include <boost/thread.hpp>
@@ -1877,8 +1878,8 @@ static bool WriteUndoDataForBlock(const CBlockUndo& blockundo, CValidationState&
 
 static CCheckQueue<CScriptCheck> scriptcheckqueue(128);
 
-void ThreadScriptCheck() {
-    RenameThread("auroracoin-scriptch");
+void ThreadScriptCheck(int worker_num) {
+    util::ThreadRename(strprintf("scriptch.%i", worker_num));
     scriptcheckqueue.Thread();
 }
 
