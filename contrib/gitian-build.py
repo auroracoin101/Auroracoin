8f1d3d18b81638cedbba077f5abcda8782244a68
Myckel Habets <myckel@sdf.org>
2020-06-29 09:45:39 +0200

    Bitcoin: fa193dc8e6f3b96fa2dba2f1c1668f7720fed320 (doc: Remove win32 from the release process).

--- contrib/gitian-build.py
+++ contrib/gitian-build.py
@@ -98,7 +98,6 @@ def sign():
         subprocess.check_call(['bin/gbuild', '--skip-image', '--upgrade', '--commit', 'signature='+args.commit, '../auroracoin/contrib/gitian-descriptors/gitian-win-signer.yml'])
         subprocess.check_call(['bin/gsign', '-p', args.sign_prog, '--signer', args.signer, '--release', args.version+'-win-signed', '--destination', '../gitian.sigs/', '../Auroracoin/contrib/gitian-descriptors/gitian-win-signer.yml'])
         subprocess.check_call('mv build/out/auroracoin-*win64-setup.exe ../auroracoin-binaries/'+args.version, shell=True)
-        subprocess.check_call('mv build/out/auroracoin-*win32-setup.exe ../auroracoin-binaries/'+args.version, shell=True)
 
     if args.macos:
         print('\nSigning ' + args.version + ' MacOS')
