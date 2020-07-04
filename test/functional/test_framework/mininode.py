2d27d7fb1f4959323aca08dcc98733d87d74add8
Myckel Habets <myckel@sdf.org>
2020-07-04 21:17:16 +0200

    Remove dandelion tests.

--- test/functional/test_framework/mininode.py
+++ test/functional/test_framework/mininode.py
@@ -62,7 +62,6 @@ MESSAGEMAP = {
     b"block": msg_block,
     b"blocktxn": msg_blocktxn,
     b"cmpctblock": msg_cmpctblock,
-    b"dandeliontx": msg_dandeliontx,
     b"feefilter": msg_feefilter,
     b"getaddr": msg_getaddr,
     b"getblocks": msg_getblocks,
@@ -320,7 +319,6 @@ class P2PInterface(P2PConnection):
     def on_block(self, message): pass
     def on_blocktxn(self, message): pass
     def on_cmpctblock(self, message): pass
-    def on_dandeliontx(self, message): pass
     def on_feefilter(self, message): pass
     def on_getaddr(self, message): pass
     def on_getblocks(self, message): pass
