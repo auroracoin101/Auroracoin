5d8e0c5f513cd6969420c6229980df1bf8a74612
Myckel Habets <myckel@sdf.org>
2020-09-25 13:43:46 +0200

    MacOSX installer/dmg-file buiding fixes.

--- contrib/macdeploy/custom_dsstore.py
+++ contrib/macdeploy/custom_dsstore.py
@@ -44,8 +44,8 @@ alias = Alias.from_bytes(icvp['backgroundImageAlias'])
 alias.volume.name = package_name_ns
 alias.volume.posix_path = '/Volumes/' + package_name_ns
 alias.volume.disk_image_alias.target.filename = package_name_ns + '.temp.dmg'
-alias.volume.disk_image_alias.target.carbon_path = 'Macintosh HD:Users:\x00digibyteuser:\x00Documents:\x00digibyte:\x00digibyte:\x00' + package_name_ns + '.temp.dmg'
-alias.volume.disk_image_alias.target.posix_path = 'Users/digibyteuser/Documents/digibyte/digibyte/' + package_name_ns + '.temp.dmg'
+alias.volume.disk_image_alias.target.carbon_path = 'Macintosh HD:Users:\x00auroracoinuser:\x00Documents:\x00auroracoin:\x00auroracoin:\x00' + package_name_ns + '.temp.dmg'
+alias.volume.disk_image_alias.target.posix_path = 'Users/auroracoinuser/Documents/auroracoin/auroracoin/' + package_name_ns + '.temp.dmg'
 alias.target.carbon_path = package_name_ns + ':.background:\x00background.tiff'
 icvp['backgroundImageAlias'] = biplist.Data(alias.to_bytes())
 ds['.']['icvp'] = icvp
@@ -53,7 +53,7 @@ ds['.']['icvp'] = icvp
 ds['.']['vSrn'] = ('long', 1)
 
 ds['Applications']['Iloc'] = (370, 156)
-ds['DigiByte-Qt.app']['Iloc'] = (128, 156)
+ds['Auroracoin-Qt.app']['Iloc'] = (128, 156)
 
 ds.flush()
 ds.close()
