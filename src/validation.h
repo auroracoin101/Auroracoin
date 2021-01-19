47d3d226cd4bf0a8dd5e22d45358ccef1eaeac5e
Myckel Habets <myckel@sdf.org>
2021-01-19 10:14:43 +0100

    Bitcoin: 8053e5cdade87550f0381d51feab81dedfec6c46 (Remove -mempoolreplacement to prevent needless block prop slowness.).

--- src/validation.h
+++ src/validation.h
@@ -122,8 +122,6 @@ static const bool DEFAULT_SPENTINDEX = false;
 static const unsigned int DEFAULT_BANSCORE_THRESHOLD = 100;
 /** Default for -persistmempool */
 static const bool DEFAULT_PERSIST_MEMPOOL = true;
-/** Default for -mempoolreplacement */
-static const bool DEFAULT_ENABLE_REPLACEMENT = true;
 /** Default for using fee filter */
 static const bool DEFAULT_FEEFILTER = true;
 
@@ -164,7 +162,6 @@ extern size_t nCoinCacheUsage;
 extern CFeeRate minRelayTxFee;
 /** If the tip is older than this (in seconds), the node is considered to be in initial block download. */
 extern int64_t nMaxTipAge;
-extern bool fEnableReplacement;
 
 /** Block hash whose ancestors we will assume to have valid scripts without checking them. */
 extern uint256 hashAssumeValid;
