2e0fecc945feda6e788ca4844d70bfa9f50f258e
Myckel Habets <myckel@sdf.org>
2021-01-28 14:54:23 +0100

    Bitcoin: 96fd4ee02f6c3be21cade729b95a85c60634b0f8 (Add MSG_NOPREFIX flag for user messages).

--- src/qt/auroracoingui.cpp
+++ src/qt/auroracoingui.cpp
@@ -1063,20 +1063,21 @@ void AuroracoinGUI::message(const QString& title, QString message, unsigned int 
     int nMBoxIcon = QMessageBox::Information;
     int nNotifyIcon = Notificator::Information;
 
-    QString msgType;
+    bool prefix = !(style & CClientUIInterface::MSG_NOPREFIX);
+    style &= ~CClientUIInterface::MSG_NOPREFIX;
 
-    // Prefer supplied title over style based title
+    QString msgType;
     if (!title.isEmpty()) {
         msgType = title;
     } else {
         switch (style) {
         case CClientUIInterface::MSG_ERROR:
             msgType = tr("Error");
-            message = tr("Error: %1").arg(message);
+            if (prefix) message = tr("Error: %1").arg(message);
             break;
         case CClientUIInterface::MSG_WARNING:
             msgType = tr("Warning");
-            message = tr("Warning: %1").arg(message);
+            if (prefix) message = tr("Warning: %1").arg(message);
             break;
         case CClientUIInterface::MSG_INFORMATION:
             msgType = tr("Information");
