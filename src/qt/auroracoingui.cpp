00c00d05053394b3771e057042d9f39056bfc6fc
Myckel Habets <myckel@sdf.org>
2021-01-28 11:52:30 +0100

    Bitcoin: f0641f274ffe94fbe7cae43c07a9373013739df2 (Prepend the error/warning prefix for GUI messages).

--- src/qt/auroracoingui.cpp
+++ src/qt/auroracoingui.cpp
@@ -1055,9 +1055,10 @@ void AuroracoinGUI::setNumBlocks(int count, const QDateTime& blockDate, double n
     progressBar->setToolTip(tooltip);
 }
 
-void AuroracoinGUI::message(const QString &title, const QString &message, unsigned int style, bool *ret)
+void AuroracoinGUI::message(const QString& title, QString message, unsigned int style, bool* ret)
 {
-    QString strTitle = tr("Auroracoin"); // default title
+    // Default title. On macOS, the window title is ignored (as required by the macOS Guidelines).
+    QString strTitle{PACKAGE_NAME};
     // Default to information icon
     int nMBoxIcon = QMessageBox::Information;
     int nNotifyIcon = Notificator::Information;
@@ -1067,37 +1068,37 @@ void AuroracoinGUI::message(const QString &title, const QString &message, unsign
     // Prefer supplied title over style based title
     if (!title.isEmpty()) {
         msgType = title;
-    }
-    else {
+    } else {
         switch (style) {
         case CClientUIInterface::MSG_ERROR:
             msgType = tr("Error");
+            message = tr("Error: %1").arg(message);
             break;
         case CClientUIInterface::MSG_WARNING:
             msgType = tr("Warning");
+            message = tr("Warning: %1").arg(message);
             break;
         case CClientUIInterface::MSG_INFORMATION:
             msgType = tr("Information");
+            // No need to prepend the prefix here.
             break;
         default:
             break;
         }
     }
-    // Append title to "Auroracoin - "
-    if (!msgType.isEmpty())
-        strTitle += " - " + msgType;
 
-    // Check for error/warning icon
+    if (!msgType.isEmpty()) {
+        strTitle += " - " + msgType;
+    }
+
     if (style & CClientUIInterface::ICON_ERROR) {
         nMBoxIcon = QMessageBox::Critical;
         nNotifyIcon = Notificator::Critical;
-    }
-    else if (style & CClientUIInterface::ICON_WARNING) {
+    } else if (style & CClientUIInterface::ICON_WARNING) {
         nMBoxIcon = QMessageBox::Warning;
         nNotifyIcon = Notificator::Warning;
     }
 
-    // Display message
     if (style & CClientUIInterface::MODAL) {
         // Check for buttons, use OK as default, if none was supplied
         QMessageBox::StandardButton buttons;
@@ -1110,9 +1111,9 @@ void AuroracoinGUI::message(const QString &title, const QString &message, unsign
         int r = mBox.exec();
         if (ret != nullptr)
             *ret = r == QMessageBox::Ok;
-    }
-    else
+    } else {
         notificator->notify(static_cast<Notificator::Class>(nNotifyIcon), strTitle, message);
+    }
 }
 
 void AuroracoinGUI::changeEvent(QEvent *e)
