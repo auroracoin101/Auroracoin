da678b1993bb78bfa681e38ea8b381e98c14220d
Myckel Habets <myckel@sdf.org>
2021-02-26 09:26:10 +0100

    Bitcoin: 68f546635d5de2ccfedadeabc7bc79e12e5eca6a (test: Fix local variable 'e' is assigned to but never used).

--- test/functional/feature_assumevalid.py
+++ test/functional/feature_assumevalid.py
@@ -73,7 +73,7 @@ class AssumeValidTest(DigiByteTestFramework):
                 break
             try:
                 p2p_conn.send_message(msg_block(self.blocks[i]))
-            except IOError as e:
+            except IOError:
                 assert not p2p_conn.is_connected
                 break
 
