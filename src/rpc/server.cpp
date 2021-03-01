4e037e1a1676d0c09d6d393fcb1502d0afcb9532
Myckel Habets <myckel@sdf.org>
2021-03-01 10:24:04 +0100

    Bitcoin: e90478f43e7bf9726ba033fde4a2776f9d5a9af4 (log: harmonize bitcoind server logging).

--- src/rpc/server.cpp
+++ src/rpc/server.cpp
@@ -164,7 +164,7 @@ UniValue stop(const JSONRPCRequest& jsonRequest)
     if (jsonRequest.fHelp || jsonRequest.params.size() > 0)
         throw std::runtime_error(
             RPCHelpMan{"stop",
-                "\nStop Auroracoin server.",
+                "\nRequest a graceful shutdown of " PACKAGE_NAME ".",
                 {},
                 RPCResults{},
                 RPCExamples{""},
@@ -175,7 +175,7 @@ UniValue stop(const JSONRPCRequest& jsonRequest)
     if (jsonRequest.params[0].isNum()) {
         MilliSleep(jsonRequest.params[0].get_int());
     }
-    return "Auroracoin server stopping";
+    return PACKAGE_NAME " stopping";
 }
 
 static UniValue uptime(const JSONRPCRequest& jsonRequest)
