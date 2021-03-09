b0a83991d2feb6ceb76afa3e69b46ca4ad912c0e
Myckel Habets <myckel@sdf.org>
2021-03-09 10:32:12 +0100

    Bitcoin: 59373e3e94015316bcaa03a7b9c2e6f442641720 (refactor: replace qSort with std::sort).

--- src/qt/addresstablemodel.cpp
+++ src/qt/addresstablemodel.cpp
@@ -11,6 +11,8 @@
 #include <key_io.h>
 #include <wallet/wallet.h>
 
+#include <algorithm>
+
 #include <QFont>
 #include <QDebug>
 
@@ -90,7 +92,7 @@ public:
         // qLowerBound() and qUpperBound() require our cachedAddressTable list to be sorted in asc order
         // Even though the map is already sorted this re-sorting step is needed because the originating map
         // is sorted by binary address, not by base58() address.
-        qSort(cachedAddressTable.begin(), cachedAddressTable.end(), AddressTableEntryLessThan());
+        std::sort(cachedAddressTable.begin(), cachedAddressTable.end(), AddressTableEntryLessThan());
     }
 
     void updateEntry(const QString &address, const QString &label, bool isMine, const QString &purpose, int status)
