24e781bbd09ef7a22c4664ae168dad7d65d64616
Myckel Habets <myckel@sdf.org>
2021-03-17 12:05:36 +0100

    Bitcoin: faaee1e39a91b3f603881655d3980c29af09852b (test: Use connect_nodes when connecting nodes in the test_framework).

--- test/functional/p2p_disconnect_ban.py
+++ test/functional/p2p_disconnect_ban.py
@@ -19,6 +19,10 @@ class DisconnectBanTest(DigiByteTestFramework):
         self.num_nodes = 2
 
     def run_test(self):
+        self.log.info("Connect nodes both way")
+        connect_nodes(self.nodes[0], 1)
+        connect_nodes(self.nodes[1], 0)
+
         self.log.info("Test setban and listbanned RPCs")
 
         self.log.info("setban: successfully ban single IP address")
@@ -75,7 +79,9 @@ class DisconnectBanTest(DigiByteTestFramework):
 
         # Clear ban lists
         self.nodes[1].clearbanned()
-        connect_nodes_bi(self.nodes, 0, 1)
+        self.log.info("Connect nodes both way")
+        connect_nodes(self.nodes[0], 1)
+        connect_nodes(self.nodes[1], 0)
 
         self.log.info("Test disconnectnode RPCs")
 
